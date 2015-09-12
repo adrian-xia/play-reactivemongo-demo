1808feab4426f0547954322fafcdb27a7d592f64
adrian-xia <adrian_xia@163.com>
2015-09-12 10:08:39 +0800

    111

--- demo-xl/app/controllers/UserAction.scala
+++ demo-xl/app/controllers/UserAction.scala
@@ -1,19 +1,53 @@
 package controllers
 
+import javax.inject.Inject
+import models.User
+import models.User._
 import play.api._
+import play.api.libs.json.Json
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
 import play.api.mvc._
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.ReactiveMongoApi
+import play.modules.reactivemongo.ReactiveMongoComponents
+import play.modules.reactivemongo.json._
+import play.modules.reactivemongo.json.ImplicitBSONHandlers._
+import play.modules.reactivemongo.json.collection._
+import reactivemongo.api.gridfs._
+import play.mvc.Results.Redirect
+import play.api.i18n.MessagesApi
 
 /**
  * @author adrian
  */
-class UserAction extends Controller{
+class UserAction @Inject()( val messagesApi: MessagesApi, val reactiveMongoApi: ReactiveMongoApi ) 
+            extends Controller with MongoController with ReactiveMongoComponents{
+  
+  
+  import java.util.UUID
+  
+  type JSONReadFile = ReadFile[JSONSerializationPack.type, JsString]
+  
+  val collection = db[JSONCollection]("users")
+    
   
   def regist = Action {
     Ok(views.html.regist("注册页面!"))
   }
   
   def login = Action {
-    Ok(views.html.login("登陆页面!"))
+    Ok(views.html.login(models.form.LoginForm.loginForm))
+  }
+  
+  def loginSubmit = Action { implicit request =>
+    val username = request.getQueryString("username")
+    val query = Json.obj("username"->username)
+    val user = collection.find(query)
+    
+    play.Logger.debug("user = " + user)
+    
+    Ok(views.html.login(models.form.LoginForm.loginForm))
   }
   
 }
