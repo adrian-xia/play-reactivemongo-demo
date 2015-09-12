131b4bf448e203018edffe195ecf7189eceb5b03
adrian-xia <adrian_xia@163.com>
2015-09-12 22:14:30 +0800

    增加转向编辑页面，增加删除方法

--- demo/app/controllers/Users.scala
+++ demo/app/controllers/Users.scala
@@ -38,21 +38,28 @@ class Users @Inject()(
   type JSONReadFile = ReadFile[JSONSerializationPack.type, JsString]
   
   val collection = db[JSONCollection]("users")
+  
   private val gridFS = reactiveMongoApi.gridFS
   
   gridFS.ensureIndex().onComplete {
     case index =>
       Logger.info(s"Checked index, result is $index")
   }
-  
+  /**
+   * 登陆页面
+   */
   def showLoginPage = Action { request =>
     implicit val messages = messagesApi.preferred(request)
 
     Ok(views.html.login(LoginForm.form))
   }
-  
+  /**
+   * 登陆方法
+   */
   def loginSubmit = TODO
-  
+  /**
+   * 展示所有用户列表
+   */
   def listUsers = Action.async { implicit request =>
     
     val query = Json.obj("$query" -> Json.obj())
@@ -67,19 +74,23 @@ class Users @Inject()(
         BadRequest(e.getMessage())
     }
   }
-  
+  /**
+   * 注册页面
+   */
   def showRegistPage = Action { request =>
     implicit val messages = messagesApi.preferred(request)
 
-    Ok(views.html.regist(User.form))
+    Ok(views.html.regist(null, User.form))
   }
-  
+  /**
+   * 注册方法，保存用户
+   */
   def registSubmit = Action.async { implicit request =>
     implicit val messages = messagesApi.preferred(request)
-
+    
     User.form.bindFromRequest.fold(
       errors => Future.successful(
-        Ok(views.html.regist(errors))),
+        Ok(views.html.regist(null, errors))),
 
       // 没有验证错误，插入注册用户
       user => collection.insert(user.copy(
@@ -88,6 +99,41 @@ class Users @Inject()(
         updatetime = Some(new DateTime()))
       ).map(_ => Redirect(routes.Users.showLoginPage))
     )
+  }
+  
+  /**
+   * 删除用户
+   */
+  def deleteUser(id: String) = Action.async {
+    gridFS.find[JsObject, JSONReadFile](Json.obj("user" -> id)).
+      collect[List]().flatMap { files =>
+        val deletions = files.map(gridFS.remove(_))
+        Future.sequence(deletions)
+      }.flatMap { _ =>
+        collection.remove(Json.obj("_id" -> id))
+      }.map(_ => Ok).recover { case _ => InternalServerError }
+  }
+  /**
+   * 转向编辑页面
+   */
+  def showEditPage(id: String) = Action.async { request =>
+    val futureUser = collection.find(Json.obj("_id" -> id)).one[User]
+    for {
+      maybeUser <- futureUser
+      result <- maybeUser.map { user =>
+        gridFS.find[JsObject, JSONReadFile](
+          Json.obj("user" -> user.id.get)).collect[List]().map { files =>
+          val filesWithId = files.map { file =>
+            file.id -> file
+          }
+
+          implicit val messages = messagesApi.preferred(request)
+          
+          Ok(views.html.regist(Some(id),
+            User.form.fill(user)))
+        }
+      }.getOrElse(Future(NotFound))
+    } yield result
   }
   
 }
